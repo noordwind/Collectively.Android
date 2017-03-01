3d0e7c0edaba8d77eae93697352b49b05b9a1fa7
snuffix <adrian.kremski@gmail.com>
2017-03-01 04:16:19 +0100

    Switcher added to profile

--- app/src/main/java/pl/adriankremski/coolector/profile/ProfileActivity.kt
+++ app/src/main/java/pl/adriankremski/coolector/profile/ProfileActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
-import android.support.v7.widget.Toolbar
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.RelativeSizeSpan
@@ -13,15 +12,19 @@ import android.text.style.StyleSpan
 import android.view.Menu
 import android.view.View
 import android.view.animation.AlphaAnimation
-import android.widget.LinearLayout
-import android.widget.TextView
 import android.widget.Toast
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.view_error.*
+import kotlinx.android.synthetic.main.view_progress.*
 import pl.adriankremski.coolector.BaseActivity
 import pl.adriankremski.coolector.R
 import pl.adriankremski.coolector.TheApp
 import pl.adriankremski.coolector.model.Profile
 import pl.adriankremski.coolector.repository.ProfileRepository
+import pl.adriankremski.coolector.utils.RequestErrorDecorator
+import pl.adriankremski.coolector.utils.Switcher
+import java.util.*
 import javax.inject.Inject
 
 
@@ -50,11 +53,8 @@ class ProfileActivity : BaseActivity(), ProfileMvp.View, AppBarLayout.OnOffsetCh
     private var mIsTheTitleVisible = false
     private var mIsTheTitleContainerVisible = true
 
-    private lateinit var mTitleContainer: LinearLayout
-    private lateinit var mTitle: TextView
-    private lateinit var mAppBarLayout: AppBarLayout
-    private lateinit var mToolbar: Toolbar
-
+    private lateinit var mSwitcher: Switcher
+    private lateinit var mErrorDecorator: RequestErrorDecorator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,50 +63,53 @@ class ProfileActivity : BaseActivity(), ProfileMvp.View, AppBarLayout.OnOffsetCh
         var span = SpannableString(getString(R.string.profile_screen_title))
         span.setSpan(RelativeSizeSpan(1.2f), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         span.setSpan(StyleSpan(Typeface.BOLD), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-//        mToolbarTitleLabel.text = span;
+        mToolbarTitleLabel.text = span;
         mCompositeDisposable = CompositeDisposable();
+
+        mAppBar.addOnOffsetChangedListener(this);
+        mToolbar.inflateMenu(R.menu.menu_main);
+        startAlphaAnimation(mToolbarTitleLabel, 0, View.INVISIBLE);
+        mErrorDecorator = RequestErrorDecorator(mSwitcherErrorImage, mSwitcherErrorTitle, mSwitcherErrorFooter)
+        setupSwitcher()
 
         mPresenter = ProfilePresenter(this, mProfileRepository)
         mPresenter.loadProfile()
-
-        bindActivity();
-
-        mAppBarLayout.addOnOffsetChangedListener(this);
-
-        mToolbar.inflateMenu(R.menu.menu_main);
-        startAlphaAnimation(mTitle, 0, View.INVISIBLE);
     }
 
-    private fun bindActivity() {
-        mToolbar = findViewById(R.id.main_toolbar) as Toolbar
-        mTitle = findViewById(R.id.main_textview_title) as TextView
-        mTitleContainer = findViewById(R.id.main_linearlayout_title) as LinearLayout
-        mAppBarLayout = findViewById(R.id.main_appbar) as AppBarLayout
+    private fun setupSwitcher() {
+        val contentViews = LinkedList<View>()
+        contentViews.add(mContent)
+        mSwitcher = Switcher.Builder()
+                .withContentViews(contentViews)
+                .withErrorViews(listOf<View>(mSwitcherError))
+                .withProgressViews(listOf<View>(mSwitcherProgress))
+                .build(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        getMenuInflater().inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun showLoading() {
-        Toast.makeText(this, "Loading", Toast.LENGTH_SHORT).show()
+        mSwitcher.showProgressViewsImmediately()
     }
 
     override fun showLoadProfileError(message: String?) {
-        Toast.makeText(this, "Loading error", Toast.LENGTH_SHORT).show()
+        mSwitcher.showErrorViewsImmediately()
     }
 
     override fun showProfile(profile: Profile) {
+        mSwitcher.showContentViewsImmediately()
         Toast.makeText(this, "Profile loaded", Toast.LENGTH_SHORT).show()
     }
 
     override fun showLoadProfileNetworkError() {
-        Toast.makeText(this, "Network error", Toast.LENGTH_SHORT).show()
+        mSwitcher.showErrorViewsImmediately()
     }
 
     override fun onOffsetChanged(appBarLayout: AppBarLayout, offset: Int) {
-        val maxScroll = appBarLayout.getTotalScrollRange()
+        val maxScroll = appBarLayout.totalScrollRange
         val percentage = Math.abs(offset).toFloat() / maxScroll.toFloat()
 
         handleAlphaOnTitle(percentage)
@@ -117,14 +120,14 @@ class ProfileActivity : BaseActivity(), ProfileMvp.View, AppBarLayout.OnOffsetCh
         if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
 
             if (!mIsTheTitleVisible) {
-                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE)
+                startAlphaAnimation(mToolbarTitleLabel, ALPHA_ANIMATIONS_DURATION, View.VISIBLE)
                 mIsTheTitleVisible = true
             }
 
         } else {
 
             if (mIsTheTitleVisible) {
-                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE)
+                startAlphaAnimation(mToolbarTitleLabel, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE)
                 mIsTheTitleVisible = false
             }
         }
