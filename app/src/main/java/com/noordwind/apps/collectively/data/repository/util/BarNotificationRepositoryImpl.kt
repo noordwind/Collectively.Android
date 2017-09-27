6d02a6f0376c1ac5f159c2c119e6d35b7ff81ba9
snuffix <adrian.kremski@gmail.com>
2017-09-27 20:32:35 +0200

    Fixes

--- app/src/main/java/com/noordwind/apps/collectively/data/repository/util/BarNotificationRepositoryImpl.kt
+++ app/src/main/java/com/noordwind/apps/collectively/data/repository/util/BarNotificationRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.noordwind.apps.collectively.data.repository.util
 import android.app.Notification
 import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import android.support.v4.app.NotificationCompat
 
 
@@ -12,9 +13,11 @@ class BarNotificationRepositoryImpl(val context: Context) : BarNotificationRepos
 
     override fun showProgressNotification(tag: String, smallIconResource: Int, title: String, message: String?) {
         var notificationBuilder = NotificationCompat.Builder(context)
-        notificationBuilder.setContentTitle(title)
-                .setProgress(0, 0, true)
-//                .setSmallIcon(smallIconResource)
+        notificationBuilder.setContentTitle(title).setProgress(0, 0, true)
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            notificationBuilder.setContentTitle(title).setSmallIcon(smallIconResource)
+        }
 
         message?.let {
             notificationBuilder.setContentText(message)
@@ -27,7 +30,10 @@ class BarNotificationRepositoryImpl(val context: Context) : BarNotificationRepos
         var notificationBuilder = NotificationCompat.Builder(context)
 
         notificationBuilder.setContentTitle(title)
-//                .setSmallIcon(smallIconResource)
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            notificationBuilder.setContentTitle(title).setSmallIcon(smallIconResource)
+        }
 
         message?.let {
             notificationBuilder.setContentText(message)
